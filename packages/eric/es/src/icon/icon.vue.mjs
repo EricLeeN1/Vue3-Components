8df81d3f7365f888a87dd56e5bda1b36995f2d6c
EricLeeN1 <179310009@qq.com>
2023-02-22 09:01:50 +0800

    feac:'样式及目录更新'

--- packages/eric/es/src/icon/icon.vue.mjs
+++ packages/eric/es/src/icon/icon.vue.mjs
@@ -1,8 +1,9 @@
 import { defineComponent, openBlock, createElementBlock } from "vue";
 import "./style/index.scss.mjs";
 const _hoisted_1 = { class: "ea-icon" };
+const __default__ = defineComponent({ name: "ea-icon" });
 const _sfc_main = /* @__PURE__ */ defineComponent({
-  name: "icon",
+  ...__default__,
   setup(__props) {
     return (_ctx, _cache) => {
       return openBlock(), createElementBlock("div", _hoisted_1, "Icon");
--- packages/eric/lib/src/icon/icon.vue.js
+++ packages/eric/lib/src/icon/icon.vue.js
@@ -3,8 +3,9 @@ Object.defineProperties(exports, { __esModule: { value: true }, [Symbol.toString
 const vue = require("vue");
 require("./style/index.scss.js");
 const _hoisted_1 = { class: "ea-icon" };
+const __default__ = vue.defineComponent({ name: "ea-icon" });
 const _sfc_main = /* @__PURE__ */ vue.defineComponent({
-  name: "icon",
+  ...__default__,
   setup(__props) {
     return (_ctx, _cache) => {
       return vue.openBlock(), vue.createElementBlock("div", _hoisted_1, "Icon");
